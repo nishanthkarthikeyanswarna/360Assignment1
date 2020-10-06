53ab6901c348c0b0eddd00b318e873b3098d53ef
Nishanth Karthikeyan Swarna <nishanthskarthik@gmail.com>
2020-10-05 18:56:28 -0700

    Initial commit

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,33 @@
+package cse360assignment02;
+
+// class name
+public class AddingMachine {
+    private int total;
+
+    //run the machine
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    //gets the total
+    public int getTotal () {
+        return 0;
+    }
+
+    //adds
+    public void add (int value) {
+    }
+
+    //subtracts
+    public void subtract (int value) {
+    }
+
+    //override tostring
+    public String toString () {
+        return "";
+    }
+
+    //clears
+    public void clear() {
+    }
+}
