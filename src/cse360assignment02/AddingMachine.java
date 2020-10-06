ca58701caf100e97b2103a28e72b8d005c3a5d8b
Nishanth Karthikeyan Swarna <nishanthskarthik@gmail.com>
2020-10-05 19:06:09 -0700

    create the second version

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -3,31 +3,38 @@ package cse360assignment02;
 // class name
 public class AddingMachine {
     private int total;
+    private String str;
 
     //run the machine
-    public AddingMachine () {
-        total = 0;  // not needed - included for clarity
+    public AddingMachine() {
+        total = 0;
+        str = "0";
     }
 
     //gets the total
-    public int getTotal () {
-        return 0;
+    public int getTotal() {
+        return total;
     }
 
     //adds
-    public void add (int value) {
+    public void add(int value) {
+        total += value;
+        str += " + " + value;
     }
 
     //subtracts
-    public void subtract (int value) {
+    public void subtract(int value) {
+        total -= value;
+        str += " - " + value;
     }
 
     //override tostring
-    public String toString () {
-        return "";
+    public String toString() {
+        return str;
     }
 
     //clears
     public void clear() {
+        total = 0;
     }
 }
